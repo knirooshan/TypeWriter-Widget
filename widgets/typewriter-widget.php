d944e5cc464c5e06b2e7f1a233567555870bd96c
Kaveesha Nirooshan <Kaveeshanirooshan2003@gmail.com>
2023-10-02 19:26:55 +0530

    Update typewriter-widget.php

--- widgets/typewriter-widget.php
+++ widgets/typewriter-widget.php
@@ -135,7 +135,7 @@ class Elementor_typewriter_Widget extends \Elementor\Widget_Base {
 		$settings = $this->get_settings_for_display();
 		$html = wp_oembed_get( $settings['url'] );
 
-		echo '<div class="oembed-elementor-widget">';
+		echo '<div class="typewriter widget">';
 		echo ( $html ) ? $html : $settings['url'];
 		echo '</div>';
 
